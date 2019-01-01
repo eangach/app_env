78f2de15f62eae4f0f61ffad062c8f02ff729bae
Thomas Behrndt <thomas.behrndt@me.com>
2019-01-02 08:57:29 +1030

    Refactor db rake tasks

---
+++ app_env_db_rake_tasks.rb
@@ -0,0 +1,39 @@
+# Assumes have database set in ENV
+
+require "rom/sql/rake_task"
+require 'sequel'
+
+require_relative 'app_env_db_config'
+database_url = ENV['DATABASE_URL']
+app_db_config = AppEnv::DB::Config.new(database_url)
+
+namespace :db do
+  desc 'Create database'
+  task :create do
+    Sequel.connect(app_db_config.to_h.merge(database: :postgres)) do |db|
+      db.execute "DROP DATABASE IF EXISTS #{app_db_config.database}"
+      db.execute "CREATE DATABASE #{app_db_config.database}"
+    end
+  end
+
+  desc 'Drop database'
+  task :drop do
+    Sequel.connect(app_db_config.to_h.merge(database: :postgres)) do |db|
+      db.execute "DROP DATABASE IF EXISTS #{app_db_config.database}"
+    end
+  end
+end
+
+# Remnove the some database tasks from production
+# # from https://stackoverflow.com/questions/31855366/rake-list-remove-custom-rake-task-in-specific-environment-development-pro
+if AppEnv.production?
+  tasks_to_disable = [
+    "db:clean",
+    "db:create",
+    "db:create_migration",
+    "db:drop",
+    "db:reset"
+  ]
+  tasks = Rake.application.instance_variable_get("@tasks")
+  tasks.delete_if { |task_name, _| tasks_to_disable.include?(task_name) }
+end
